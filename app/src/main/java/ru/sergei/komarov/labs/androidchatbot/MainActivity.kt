f919aab7d76110b5e3cc05e0a172bb0f167094ef
JayLim <ddx180874@gmail.com>
2019-10-22 22:01:46 +0400

    start of project

---
+++ app/src/main/java/ru/sergei/komarov/labs/androidchatbot/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.sergei.komarov.labs.androidchatbot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
